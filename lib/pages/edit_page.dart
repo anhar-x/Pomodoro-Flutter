9fc99d1ab8ec22b7f000bb1271e702a0f05cf32e
Anhar <anharx@gmail.com>
2021-02-24 17:25:01 +0530

    Better design in Edit Page

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -20,19 +20,10 @@ class _EditPageState extends State<EditPage> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
       ),
-      color: Colors.pink,
+      color: Colors.blueGrey,
       child: Column(
         children: [
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            color: Colors.pink[300],
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Text('$name \n DURATION', style: TextStyle(fontSize: 24)),
-            ),
-          ),
+          Text('$name \n DURATION', style: TextStyle(fontSize: 24)),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -84,120 +75,6 @@ class _EditPageState extends State<EditPage> {
         _createUI(name: 'POMODORO', timerType: pomodoro, prefsName: 'pomodoro'),
         _createUI(name: 'SHORT BREAK', timerType: shortBreak, prefsName: 'short_break'),
         _createUI(name: 'LONG BREAK', timerType: longBreak, prefsName: 'long_break'),
-        // //SHORT BREAK
-        // Card(
-        //   shape: RoundedRectangleBorder(
-        //     borderRadius: BorderRadius.circular(15.0),
-        //   ),
-        //   color: Colors.pink,
-        //   child: Column(
-        //     children: [
-        //       Card(
-        //         shape: RoundedRectangleBorder(
-        //           borderRadius: BorderRadius.circular(15.0),
-        //         ),
-        //         color: Colors.pink[300],
-        //         child: Padding(
-        //           padding: EdgeInsets.all(10),
-        //           child: Text('SHORT BREAK \n DURATION',
-        //               style: TextStyle(fontSize: 24)),
-        //         ),
-        //       ),
-        //       Row(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children: [
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: IconButton(
-        //               icon: Icon(Icons.remove),
-        //               onPressed: () async {
-        //                 if (shortBreak > 1) {
-        //                   setState(() {
-        //                     shortBreak--;
-        //                   });
-        //                   await prefs.setInt('short_break', shortBreak);
-        //                 }
-        //               },
-        //             ),
-        //           ),
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: Text('$shortBreak'),
-        //           ),
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: IconButton(
-        //               icon: Icon(Icons.add),
-        //               onPressed: () async {
-        //                 setState(() {
-        //                   shortBreak++;
-        //                 });
-        //                 await prefs.setInt('short_break', shortBreak);
-        //               },
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        // //LONG BREAK
-        // Card(
-        //   shape: RoundedRectangleBorder(
-        //     borderRadius: BorderRadius.circular(15.0),
-        //   ),
-        //   color: Colors.pink,
-        //   child: Column(
-        //     children: [
-        //       Card(
-        //         shape: RoundedRectangleBorder(
-        //           borderRadius: BorderRadius.circular(15.0),
-        //         ),
-        //         color: Colors.pink[300],
-        //         child: Padding(
-        //           padding: EdgeInsets.all(10),
-        //           child: Text('LONG BREAK \n DURATION',
-        //               style: TextStyle(fontSize: 24)),
-        //         ),
-        //       ),
-        //       Row(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children: [
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: IconButton(
-        //               icon: Icon(Icons.remove),
-        //               onPressed: () async {
-        //                 if (longBreak > 1) {
-        //                   setState(() {
-        //                     longBreak--;
-        //                   });
-        //                   await prefs.setInt('long_break', longBreak);
-        //                 }
-        //               },
-        //             ),
-        //           ),
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: Text('$longBreak'),
-        //           ),
-        //           Padding(
-        //             padding: EdgeInsets.all(20),
-        //             child: IconButton(
-        //               icon: Icon(Icons.add),
-        //               onPressed: () async {
-        //                 setState(() {
-        //                   longBreak++;
-        //                 });
-        //                 await prefs.setInt('long_break', longBreak);
-        //               },
-        //             ),
-        //           ),
-        //         ],
-        //       ),
-        //     ],
-        //   ),
-        // ),
       ],
     );
   }
