405bbb94bc6b30d9e61036da0d9246b5b01aee23
Anhar <anharx@gmail.com>
2021-03-02 12:03:11 +0530

    UI changes in edit page

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -10,7 +10,7 @@ class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit')),
+      appBar: AppBar(title: Text('Edit'), backgroundColor: Colors.blueGrey[800],),
       body: _editPage(),
     );
   }
