311f62694027f6f5564f65b9de3d9123054d5346
Anhar <anharx@gmail.com>
2021-05-05 02:23:34 +0530

    Edit page design updated

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -10,20 +10,32 @@ class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Edit'), backgroundColor: Colors.blueGrey[800],),
+      appBar: AppBar(
+        title: Text('Edit'),
+        backgroundColor: Colors.blueGrey[800],
+      ),
       body: _editPage(),
     );
   }
 
-  _createTimeChooser({String name, int timerLen, String prefsName}) {
+  _createTimeChooser({String name, int timerLen, String prefsName, bool section=false}) {
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
+        borderRadius: BorderRadius.circular(25.0),
       ),
-      color: Colors.blueGrey,
-      child: Column(
-        children: [
-          Text('$name', style: TextStyle(fontSize: 16)),
+      color: Colors.blueGrey[900],
+      child: ExpansionTile(
+        title: Text(
+          '$name',
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
+        leading: Icon(Icons.arrow_drop_down_circle),
+
+        //if section=true, trailing should show intervals not min
+        //eg: Sections 8 intervals not Sections 8 min
+        trailing: Text(section == false ? '$timerLen  min' : '$timerLen intervals',
+            style: TextStyle(fontSize: 20, color: Colors.grey[400])),
+        children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -31,6 +43,7 @@ class _EditPageState extends State<EditPage> {
                 padding: EdgeInsets.all(20),
                 child: IconButton(
                   icon: Icon(Icons.remove),
+                  color: Colors.grey[350],
                   onPressed: () async {
                     if (timerLen > 1) {
                       setState(() {
@@ -43,12 +56,13 @@ class _EditPageState extends State<EditPage> {
               ),
               Padding(
                 padding: EdgeInsets.all(5),
-                child: Text('$timerLen', style: TextStyle(fontSize: 26)),
+                child: Text('$timerLen', style: TextStyle(fontSize: 26, color: Colors.grey[350]),),
               ),
               Padding(
                 padding: EdgeInsets.all(20),
                 child: IconButton(
                   icon: Icon(Icons.add),
+                  color: Colors.grey[350],
                   onPressed: () async {
                     setState(() {
                       timerLen++;
@@ -72,10 +86,19 @@ class _EditPageState extends State<EditPage> {
 
     return Column(
       children: [
-        _createTimeChooser(name: 'POMODORO \n DURATION', timerLen: pomodoro, prefsName: 'pomodoro'),
-        _createTimeChooser(name: 'SHORT BREAK \n  DURATION', timerLen: shortBreak, prefsName: 'short_break'),
-        _createTimeChooser(name: 'LONG BREAK \n  DURATION', timerLen: longBreak, prefsName: 'long_break'),
-        _createTimeChooser(name: 'POMODOROS UNTIL \n  LONG BREAK', timerLen: untilLongBreak, prefsName: 'until_long_break'),
+        _createTimeChooser(
+            name: 'Pomodoro ', timerLen: pomodoro, prefsName: 'pomodoro'),
+        _createTimeChooser(
+            name: 'Short break',
+            timerLen: shortBreak,
+            prefsName: 'short_break'),
+        _createTimeChooser(
+            name: 'Long break', timerLen: longBreak, prefsName: 'long_break'),
+        _createTimeChooser(
+            name: 'Sections',
+            timerLen: untilLongBreak,
+            prefsName: 'until_long_break',
+            section: true),
       ],
     );
   }
